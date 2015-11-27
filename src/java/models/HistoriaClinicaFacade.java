3ecc9745458745362b7a948346548d30cae74e36
David Tayupanta <davidotb91@hotmail.com>
2015-11-27 16:46:37 -0500

    Primera Version prime face
    formularios basicos

---
+++ src/java/models/HistoriaClinicaFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import entitis.HistoriaClinica;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author david
+ */
+@Stateless
+public class HistoriaClinicaFacade extends AbstractFacade<HistoriaClinica> {
+
+    @PersistenceContext(unitName = "gestydentV01PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public HistoriaClinicaFacade() {
+        super(HistoriaClinica.class);
+    }
+    
+}
